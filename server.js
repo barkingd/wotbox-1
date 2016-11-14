98231b1f6c5d522c8ecbc23dc339fab95944f48f
slfiene <sean.fiene@nintex.com>
2016-11-14 14:23:14 -0800

    asdf

--- server.js
+++ server.js
@@ -37,3 +37,17 @@ app.use("*",function(req,res){
 app.listen(3000,function(){
   console.log("Live at Port 3000");
 });
+
+var Gpio = require('onoff').Gpio,
+sensor = new Gpio(17, 'in', 'both');
+sensor.watch(function (err, value) {
+if (err) exit(err);
+console.log(value ? 'there is some one!' : 'not anymore!');
+});
+function exit(err) {
+if (err) console.log('An error occurred: ' + err);
+sensor.unexport();
+console.log('Bye, bye!')
+process.exit();
+}
+process.on('SIGINT', exit);
