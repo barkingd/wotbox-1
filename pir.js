670d25b2162154b9f955ce6e9ce421348a515ff2
slfiene <sean.fiene@nintex.com>
2016-11-15 10:25:39 -0800

    asdf

--- pir.js
+++ pir.js
@@ -1,12 +1,9 @@
 var Gpio = require('onoff').Gpio,
 sensor = new Gpio(17, 'in', 'both');
 //# PIR.JS Sensor
-console.log("The sensor should be working!");
 sensor.watch(function (err, value) {
 if (err) exit(err);
 console.log(value ? 'there is some one!' : 'not anymore!');
- $('#motionVal').val(value);
- $('#motionVal').text("Hello!")
 });
 function exit(err) {
 if (err) console.log('An error occurred: ' + err);
