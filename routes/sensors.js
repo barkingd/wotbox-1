9dc450af088798a85d295e7891391e520f699c1d
slfiene <sean.fiene@nintex.com>
2016-11-16 20:04:41 -0800

    asdf

--- routes/sensors.js
+++ routes/sensors.js
@@ -1,26 +1,21 @@
-// Final version
 var express = require('express'),
   router = express.Router(),
   resources = require('./../resources/model');
 
 router.route('/').get(function (req, res, next) {
-  req.result = resources.pi.sensors; //#A
-  next(); //#B
+  rreq.send(resources.pi.sensors);
 });
 
 router.route('/pir').get(function (req, res, next) {
-  req.result = resources.pi.sensors.pir;
-  next();
+  res.send(resources.pi.sensors.pir);
 });
 
 router.route('/temperature').get(function (req, res, next) {
-  req.result = resources.pi.sensors.temperature;
-  next();
+  res.send(resources.pi.sensors.temperature);
 });
 
 router.route('/humidity').get(function (req, res, next) {
-  req.result = resources.pi.sensors.humidity;
-  next();
+  res.send(resources.pi.sensors.humidity);
 });
 
 module.exports = router;
