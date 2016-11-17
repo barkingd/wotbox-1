dc04a0bd790f471d19db7c1c8da3fd5f2c9f476b
slfiene <sean.fiene@nintex.com>
2016-11-16 20:29:58 -0800

    adding plugins

--- wot-server.js
+++ wot-server.js
@@ -1,5 +1,12 @@
 var httpServer = require('./servers/http'),
-resources = require('./resources/model');
+    resources = require('./resources/model');
+
+var pirPlugin = require('./plugins/internal/pirPlugin'),
+    dhtPlugin = require('./plugins/internal/DHT22SensorPlugin');
+
+pirPlugin.start({'simulate': true, 'frequency': 2000});
+dhtPlugin.start({'simulate': true, 'frequency': 10000}); 
+
 
 var server = httpServer.listen(resources.pi.port, function () {
   console.info('Your WoT Pi is up and running on port %s', 
