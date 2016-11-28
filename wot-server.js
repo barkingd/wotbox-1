9946a8d9740a0de52d45b52a99558806c47d9b75
slfiene <sean.fiene@nintex.com>
2016-11-28 13:31:55 -0800

    add web sockets

--- wot-server.js
+++ wot-server.js
@@ -1,4 +1,5 @@
 var httpServer = require('./servers/http'),
+    wsServer = require('./servers/websockets')
     resources = require('./resources/model');
 
 var pirPlugin = require('./plugins/internal/pirPlugin'),
@@ -9,6 +10,8 @@ dhtPlugin.start({'simulate': true, 'frequency': 10000});
 
 
 var server = httpServer.listen(resources.pi.port, function () {
+    console.log('HTTP server started...');
+    wsServer.listen(server);
   console.info('Your WoT Pi is up and running on port %s', 
   resources.pi.port);
 });
