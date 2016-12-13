5914dcdbacf002371f61a518fbad141f5db03db8
slfiene <sean.fiene@nintex.com>
2016-12-12 16:26:34 -0800

    adding localStorage

---
+++ scripts.js
@@ -0,0 +1,22 @@
+$(document).load(function () {
+    $('#confirm').click(function () {
+        storeSettings();
+    });
+
+    function storeSettings() {
+        if (typeof (Storage) !== "undefined") {
+            //set IP Address
+            var vIP = $('#ip').val();
+            var vPhone = $('#phone').val();
+            var vEmail = $('#email').val();
+            localStorage.setItem("ipAddress", vIP);
+            localStorage.setItem("phone", vPhone);
+            localStorage.setItem("email", vEmail);
+        } else {
+            alert("Your browser does not support Web Storage!");
+        }
+    }
+});
+
+
+
