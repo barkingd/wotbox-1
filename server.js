5ea50c643a15b3e9072265e08833abf9f9b1847c
slfiene <sean.fiene@nintex.com>
2016-12-12 18:31:51 -0800

    asdf

--- server.js
+++ server.js
@@ -3,41 +3,41 @@ var app = express();
 var router = express.Router();
 var path = __dirname + '/views/';
 
-router.use(function (req,res,next) {
+router.use(function (req, res, next) {
   console.log("/" + req.method);
   next();
 });
 
-router.get("/",function(req,res){
+router.get("/", function (req, res) {
   res.sendFile(path + "index.html");
 });
 
-router.get("/about",function(req,res){
+router.get("/about", function (req, res) {
   res.sendFile(path + "about.html");
 });
 
-router.get("/temp",function(req,res){
+router.get("/temp", function (req, res) {
   res.sendFile(path + "temp.html");
 });
 
-router.get("/motion", function(req, res){
-    res.sendFile(path + "motion.html");
+router.get("/motion", function (req, res) {
+  res.sendFile(path + "motion.html");
 });
 
-router.get("/tellme", function(req, res){
-    res.sendFile(path + "tellme.html");
+router.get("/tellme", function (req, res) {
+  res.sendFile(path + "tellme.html");
 });
 
-router.get("/settings", function(req, res){
+router.get("/settings", function (req, res) {
   res.sendFile(path + "settings.html");
 });
 
-app.use("/",router);
+app.use("/", router);
 
-app.use("*",function(req,res){
+app.use("*", function (req, res) {
   res.sendFile(path + "404.html");
 });
 
-app.listen(3000,function(){
+app.listen(3000, function () {
   console.log("Live at Port 3000");
 });
