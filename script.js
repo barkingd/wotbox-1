444e02936916ed1400ca9a10cc6d52156920ced4
slfiene <sean.fiene@nintex.com>
2016-12-13 09:39:21 -0800

    update script.js

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
 $(document).load(function () {
-    $('#confirm').click(storeSettings());
-
-    function storeSettings() {
-        if (typeof (Storage) !== "undefined") {
-            //set IP Address
-            var vIP = $('#ip').val();
-            var vPhone = $('#phone').val();
-            var vEmail = $('#email').val();
-            localStorage.setItem("ipAddress", vIP);
-            localStorage.setItem("phone", vPhone);
-            localStorage.setItem("email", vEmail);
-        } else {
-            alert("Your browser does not support Web Storage!");
+    $('#confirm').click(function () {
+        function storeSettings() {
+            if (typeof (Storage) !== "undefined") {
+                //set IP Address
+                console.log("adding values!");
+                var vIP = $('#ip').val();
+                var vPhone = $('#phone').val();
+                var vEmail = $('#email').val();
+                localStorage.setItem("ipAddress", vIP);
+                localStorage.setItem("phone", vPhone);
+                localStorage.setItem("email", vEmail);
+            } else {
+                console.warn("Your browser does not support Web Storage!");
+                alert("Your browser does not support Web Storage!");
+            }
         }
-    }
+    });
 });
 
 
